5d94510a5e38bb3ee715efe08bed6053bb19ad03
VenimirPetkov <venimir@fusionlab.uk>
2025-03-13 11:49:18 +0100

    simple config

---
+++ layerzero.simple.config.ts
@@ -0,0 +1,55 @@
+import { ExecutorOptionType } from '@layerzerolabs/lz-v2-utilities'
+import { OAppEnforcedOption, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
+import { EndpointId } from '@layerzerolabs/lz-definitions'
+import { generateConnectionsConfig } from '@layerzerolabs/metadata-tools'
+
+const holeskyContract: OmniPointHardhat = {
+    eid: EndpointId.HOLESKY_V2_TESTNET,
+    contractName: 'ZenBTCOFTAdapter',
+}
+
+const baseSepContract: OmniPointHardhat = {
+    eid: EndpointId.BASESEP_V2_TESTNET,
+    contractName: 'ZenBTCOFTAdapter',
+}
+
+const EVM_ENFORCED_OPTIONS: OAppEnforcedOption[] = [
+    {
+        msgType: 1,
+        optionType: ExecutorOptionType.LZ_RECEIVE,
+        gas: 80000,
+        value: 0,
+    },
+    {
+        msgType: 2,
+        optionType: ExecutorOptionType.LZ_RECEIVE,
+        gas: 80000,
+        value: 0,
+    },
+    {
+        msgType: 2,
+        optionType: ExecutorOptionType.COMPOSE,
+        index: 0,
+        gas: 80000,
+        value: 0,
+    },
+]
+
+export default async function () {
+    // note: pathways declared here are automatically bidirectional
+    // if you declare A,B there's no need to declare B,A
+    const connections = await generateConnectionsConfig([
+        [
+            holeskyContract, // Chain A contract
+            baseSepContract, // Chain B contract
+            [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
+            [1, 1], // [A to B confirmations, B to A confirmations]
+            [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain B enforcedOptions, Chain A enforcedOptions
+        ],
+    ])
+
+    return {
+        contracts: [{ contract: holeskyContract }, { contract: baseSepContract }],
+        connections,
+    }
+}
---
+++ zenbtc.testnet.simple.config.ts
@@ -0,0 +1,53 @@
+import {ExecutorOptionType} from '@layerzerolabs/lz-v2-utilities';
+import {OAppEnforcedOption, OmniPointHardhat} from '@layerzerolabs/toolbox-hardhat';
+import {EndpointId} from '@layerzerolabs/lz-definitions';
+import {generateConnectionsConfig} from '@layerzerolabs/metadata-tools';
+
+const holeskyContract: OmniPointHardhat = {
+    eid: EndpointId.HOLESKY_V2_TESTNET,
+    contractName: 'ZenBTCOFTAdapter',
+}
+
+const baseSepContract: OmniPointHardhat = {
+    eid: EndpointId.BASESEP_V2_TESTNET,
+    contractName: 'ZenBTCOFTAdapter',
+}
+
+const EVM_ENFORCED_OPTIONS: OAppEnforcedOption[] = [
+    {
+        msgType: 1,
+        optionType: ExecutorOptionType.LZ_RECEIVE,
+        gas: 80000,
+        value: 0,
+    },
+    {
+        msgType: 2,
+        optionType: ExecutorOptionType.LZ_RECEIVE,
+        gas: 80000,
+        value: 0,
+    },
+    {
+        msgType: 2,
+        optionType: ExecutorOptionType.COMPOSE,
+        index: 0,
+        gas: 80000,
+        value: 0,
+    },
+]
+
+export default async function () {
+    const connections = await generateConnectionsConfig([
+        [
+            holeskyContract, // srcContract
+            baseSepContract, // dstContract
+            [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
+            [1, 1], // [srcToDstConfirmations, dstToSrcConfirmations]
+            [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // [enforcedOptionsSrcToDst, enforcedOptionsDstToSrc]
+        ],
+    ])
+
+    return {
+        contracts: [{ contract: holeskyContract }, { contract: baseSepContract }],
+        connections,
+    }
+}
