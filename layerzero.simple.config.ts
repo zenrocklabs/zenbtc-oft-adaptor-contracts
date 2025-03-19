de0069448de29c651dadf586c21e29060fb0937b
Valentin Kamenarov <valentin@zenrocklabs.io>
2025-03-19 15:48:14 +0200

    feat: add Solana LZ Adapter Program and tasks

--- layerzero.simple.config.ts
+++ layerzero.simple.config.ts
@@ -13,6 +13,11 @@ const baseSepContract: OmniPointHardhat = {
     contractName: 'ZenBTCOFTAdapter',
 }
 
+const solanaContract: OmniPointHardhat = {
+    eid: EndpointId.SOLANA_V2_TESTNET,
+    address: '', // NOTE: update this with the OFTStore address.
+}
+
 const EVM_ENFORCED_OPTIONS: OAppEnforcedOption[] = [
     {
         msgType: 1,
@@ -35,6 +40,29 @@ const EVM_ENFORCED_OPTIONS: OAppEnforcedOption[] = [
     },
 ]
 
+const SOLANA_ENFORCED_OPTIONS: OAppEnforcedOption[] = [
+    {
+        msgType: 1,
+        optionType: ExecutorOptionType.LZ_RECEIVE,
+        gas: 200000,
+        value: 2500000,
+    },
+    {
+        msgType: 2,
+        optionType: ExecutorOptionType.LZ_RECEIVE,
+        gas: 200000,
+        value: 2500000,
+    },
+    {
+        // Solana options use (gas == compute units, value == lamports)
+        msgType: 2,
+        optionType: ExecutorOptionType.COMPOSE,
+        index: 0,
+        gas: 0,
+        value: 0,
+    },
+]
+
 export default async function () {
     // note: pathways declared here are automatically bidirectional
     // if you declare A,B there's no need to declare B,A
@@ -46,10 +74,24 @@ export default async function () {
             [1, 1], // [A to B confirmations, B to A confirmations]
             [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain B enforcedOptions, Chain A enforcedOptions
         ],
+        [
+            solanaContract, // Chain A contract
+            arbSepContract, // Chain B contract
+            [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
+            [32, 15], // [A to B confirmations, B to A confirmations]
+            [EVM_ENFORCED_OPTIONS, SOLANA_ENFORCED_OPTIONS], // Chain B enforcedOptions, Chain A enforcedOptions
+        ],
+        [
+            solanaContract, // Chain A contract
+            baseSepContract, // Chain B contract
+            [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
+            [32, 15], // [A to B confirmations, B to A confirmations]
+            [EVM_ENFORCED_OPTIONS, SOLANA_ENFORCED_OPTIONS], // Chain B enforcedOptions, Chain A enforcedOptions
+        ],
     ])
 
     return {
-        contracts: [{ contract: arbSepContract }, { contract: baseSepContract }],
+        contracts: [{ contract: arbSepContract }, { contract: baseSepContract }, {contract: solanaContract}],
         connections,
     }
 }
